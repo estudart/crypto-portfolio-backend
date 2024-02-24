e8796896aa520bdd9bf086c1230e8d1659ce4719
estudart <erico.studartdf@gmail.com>
2024-02-24 07:36:47 -0300

    changes to the portfolio

--- schemas/users.py
+++ schemas/users.py
@@ -1,6 +1,8 @@
 from marshmallow import Schema, fields
 
 class UserSchema(Schema):
+    """ Defines how a clietn should be returned in the api call
+    """
     id = fields.Integer()
     email = fields.String()
 
